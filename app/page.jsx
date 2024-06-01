92f74f2cd53d2dce4ae4328063f666d2aa53dc0d
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2024-05-31 22:33:04 -0700

    installed next

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>This is Afaq</h2>
+      <Button>Click Here</Button>
+    </div>
+  );
+};
+
+export default Home;
